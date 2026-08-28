3a7986eb223ba2d27c7dc6dfd44d3257333d2e31
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-28 15:24:51 -0300

    funcionou

--- aula03/exemplo3.py
+++ aula03/exemplo3.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-
 from tkinter import PhotoImage, ttk
 
 
-def centralizar_imagem(event):
-    largura_janela = janela.winfo.width()
-    altura_janela = janela.winfo.height()
+def centralizar_imagem(event=None):
+    largura_janela = janela.winfo_width()
+    altura_janela = janela.winfo_height()
+
     largura_imagem = imagem.width()
     altura_imagem = imagem.height()
 
@@ -14,20 +14,27 @@ def centralizar_imagem(event):
 
     lbl_imagem.place(x=posicao_x, y=posicao_y)
 
+
 janela = tk.Tk()
-janela.title('Exibir Imagem')   
+
+janela.title('Exibir Imagem')
 janela.geometry('400x250')
 
-'''
-Imagem de <a href="https://pixabay.com/pt/users/larchick-186124/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1136234">Lara Larina</a> por <a href="https://pixabay.com/pt//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1136234">Pixabay</a>
-'''
 
 imagem = PhotoImage(file='logo.png')
 
-lbl_imagem = ttk.Label(janela, image=imagem)
+imagem = imagem.subsample(32, 32)
+
+
+lbl_imagem = ttk.Label(
+    janela,
+    image=imagem
+)
 
 janela.bind("<Configure>", centralizar_imagem)
 
 lbl_imagem.pack()
+
+centralizar_imagem()
 
 janela.mainloop()
--- aula03/labels2.py
+++ aula03/labels2.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 janela = tk.Tk()
+
 janela.geometry('300x200')
 janela.title('Uso de Labels')
 
@@ -11,15 +12,18 @@ label1 = ttk.Label(
     font=('Helvetica', 18)
 )
 
-label1.pack(ipadx=10, ipady=30)
+label1.pack(ipadx=10, ipady=10)
+
+style = ttk.Style()
+style.configure('Azul.TLabel', foreground='blue')
 
 label2 = ttk.Label(
     janela,
     text='Texto do Label 2',
     font=('Arial', 20),
-    foreground='blue'
+    style='Azul.TLabel'
 )
 
-label2.pack(ipadx=10, ipady=60)
+label2.pack(ipadx=10, ipady=10)
 
 janela.mainloop()
